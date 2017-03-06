05b50ba1a3b74fc56f3d052cb5a499319353ec64
wangyj <wangyj@liepin.com>
2017-03-06 13:53:17 +0800

    增加自定义配置

--- lumberjack.go
+++ lumberjack.go
@@ -1,6 +1,7 @@
 package logrus_lumberjack
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
@@ -9,16 +10,100 @@ import (
 )
 
 type LumberjackHook struct {
-	Writer *lumberjack.Logger
+	Crit  *lumberjack.Logger
+	Err   *lumberjack.Logger
+	Warn  *lumberjack.Logger
+	Info  *lumberjack.Logger
+	Debug *lumberjack.Logger
 }
 
-func (hook *LumberjackHook) Write(line string) error {
-	_, err := hook.Writer.Write([]byte(line))
-	return err
-}
+const (
+	DEFAULTMAXSIZE   = 1 // megabytes
+	DEFAULTMAXBACKUP = 3
+	DEFAULTMAXAGE    = 30 //days
+)
 
-func NewLumberjackHook(lumberConfig *lumberjack.Logger) (*LumberjackHook, error) {
-	return &LumberjackHook{Writer: lumberConfig}, nil
+func NewLumberjackHook(lumber interface{}) (*LumberjackHook, error) {
+	defConfig := &lumberjack.Logger{
+		Filename:   "stdout.log",
+		MaxSize:    DEFAULTMAXSIZE, // megabytes
+		MaxBackups: DEFAULTMAXBACKUP,
+		MaxAge:     DEFAULTMAXAGE, //days
+	}
+	lumberhook := &LumberjackHook{
+		Crit:  defConfig,
+		Err:   defConfig,
+		Warn:  defConfig,
+		Info:  defConfig,
+		Debug: defConfig,
+	}
+
+	switch m := lumber.(type) {
+	case map[string]*lumberjack.Logger:
+		for k, v := range m {
+			switch k {
+			case "crit":
+				lumberhook.Crit = v
+			case "err":
+				lumberhook.Err = v
+			case "warn":
+				lumberhook.Warn = v
+			case "info":
+				lumberhook.Info = v
+			case "debug":
+				lumberhook.Debug = v
+			default:
+				return nil, errors.New(k + " does not support!")
+			}
+		}
+	case map[string]string:
+		for k, v := range m {
+			switch k {
+			case "crit":
+				lumberhook.Crit = &lumberjack.Logger{
+					Filename:   v,
+					MaxSize:    DEFAULTMAXSIZE, // megabytes
+					MaxBackups: DEFAULTMAXBACKUP,
+					MaxAge:     DEFAULTMAXAGE, //days
+				}
+			case "err":
+				lumberhook.Err = &lumberjack.Logger{
+					Filename:   v,
+					MaxSize:    DEFAULTMAXSIZE, // megabytes
+					MaxBackups: DEFAULTMAXBACKUP,
+					MaxAge:     DEFAULTMAXAGE, //days
+				}
+			case "warn":
+				lumberhook.Warn = &lumberjack.Logger{
+					Filename:   v,
+					MaxSize:    DEFAULTMAXSIZE, // megabytes
+					MaxBackups: DEFAULTMAXBACKUP,
+					MaxAge:     DEFAULTMAXAGE, //days
+				}
+			case "info":
+				lumberhook.Info = &lumberjack.Logger{
+					Filename:   v,
+					MaxSize:    DEFAULTMAXSIZE, // megabytes
+					MaxBackups: DEFAULTMAXBACKUP,
+					MaxAge:     DEFAULTMAXAGE, //days
+				}
+			case "debug":
+				lumberhook.Debug = &lumberjack.Logger{
+					Filename:   v,
+					MaxSize:    DEFAULTMAXSIZE, // megabytes
+					MaxBackups: DEFAULTMAXBACKUP,
+					MaxAge:     DEFAULTMAXAGE, //days
+				}
+			default:
+				return nil, errors.New(k + " does not support!")
+			}
+		}
+
+	default:
+		fmt.Printf("%T", lumber)
+	}
+	fmt.Println(lumberhook)
+	return lumberhook, nil
 }
 
 func (hook *LumberjackHook) Fire(entry *logrus.Entry) error {
@@ -29,20 +114,21 @@ func (hook *LumberjackHook) Fire(entry *logrus.Entry) error {
 	}
 	switch entry.Level {
 	case logrus.PanicLevel:
-		return hook.Write(line)
+		_, err = hook.Crit.Write([]byte(line))
 	case logrus.FatalLevel:
-		return hook.Write(line)
+		_, err = hook.Crit.Write([]byte(line))
 	case logrus.ErrorLevel:
-		return hook.Write(line)
+		_, err = hook.Err.Write([]byte(line))
 	case logrus.WarnLevel:
-		return hook.Write(line)
+		_, err = hook.Warn.Write([]byte(line))
 	case logrus.InfoLevel:
-		return hook.Write(line)
+		_, err = hook.Info.Write([]byte(line))
 	case logrus.DebugLevel:
-		return hook.Write(line)
+		_, err = hook.Debug.Write([]byte(line))
 	default:
 		return nil
 	}
+	return err
 }
 
 func (hook *LumberjackHook) Levels() []logrus.Level {
